6d69314fe50364f991012c5b998d9824579cc4b5
chanyong.moon <chanyong.moon@navercorp.com>
2022-11-28 04:33:56 +0900

    add comment

--- src/main/java/com/yahoconut/httpinterfaceexample/configuration/HttpConfiguration.java
+++ src/main/java/com/yahoconut/httpinterfaceexample/configuration/HttpConfiguration.java
@@ -10,4 +10,28 @@ public class HttpConfiguration {
     public WebClient client() {
         return WebClient.builder().baseUrl("http://localhost:8080").build();
     }
+// if you want to use general way, uncomment below code and remove `HttpExchangePostProcessor`.
+//    @Bean
+//    public HttpServiceProxyFactory httpServiceProxyFactory(WebClient client) {
+//        return HttpServiceProxyFactory.builder(WebClientAdapter.forClient(client)).build();
+//    }
+//
+//    @Bean
+//    public ProductService productService(HttpServiceProxyFactory httpServiceProxyFactory) {
+//        return httpServiceProxyFactory.createClient(ProductService.class);
+//    }
+//
+//    @Bean
+//    public PaymentService paymentService(HttpServiceProxyFactory httpServiceProxyFactory) {
+//        return httpServiceProxyFactory.createClient(PaymentService.class);
+//    }
+
+//    @Bean
+//    public HttpExchangeInterface interfaces(WebClient client) {
+//        HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory.builder(WebClientAdapter.forClient(client)).build();
+//        Class<? extends HttpExchangeInterface>[] interfaces = (Class<? extends HttpExchangeInterface>[]) ClassUtils.getAllInterfaces(HttpExchangeInterface.class);
+//        System.out.println(interfaces);
+//        return null;
+////        return new ArrayList<>();
+//    }
 }
